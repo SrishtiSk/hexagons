0234aefb8265934b03a0735b5dc23b7f6dbb53dd
Srishtisk <srishtisk.is16@gmail.com>
2019-10-09 00:04:40 +0530

    remove the buttons

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,20 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+//using unityStandardAssets.CrossplatformerInput;
 
 public class Player : MonoBehaviour
 {
     public float _moveSpeed = 600f;
     float movement = 0f;
-   
+
     void Update()
     {
-        movement= Input.GetAxisRaw("Horizontal");
+        movement = Input.GetAxisRaw("Horizontal");
         if (Input.GetKeyDown("escape")) Application.Quit();
     }
     private void FixedUpdate()
     {
         transform.RotateAround(Vector3.zero, Vector3.forward, movement * Time.fixedDeltaTime * -_moveSpeed);
+       
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -25,6 +27,7 @@ public class Player : MonoBehaviour
 
     public void QuitGame()
     {
-       Application.Quit();
+        Application.Quit();
     }
 }
+    
